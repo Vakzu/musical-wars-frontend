49c7c0d4d0adbb6f6ff6afe2a1b32cc269645c12
dmittrey <dmitryzubakhin@mail.ru>
2022-12-23 00:04:15 +0300

    Add hand to receive online users

--- src/API/UserApi.ts
+++ src/API/UserApi.ts
@@ -1,8 +1,12 @@
-import { UserBalanceResponse } from "../types/User";
+import { UserBalanceResponse, UserOnlineResponse } from "../types/User";
 import { api } from "./api";
 
 export class UserApi {
   static getBalance = () => {
     return api.get<UserBalanceResponse>("/user/balance");
+  };
+
+  static getOnlineUsers = () => {
+    return api.get<UserOnlineResponse>("/user/online");
   };
 }
--- src/types/User.ts
+++ src/types/User.ts
@@ -1,3 +1,7 @@
 export interface UserBalanceResponse {
     balance: string
 }
+
+export interface UserOnlineResponse {
+    userNames: string[]
+}
