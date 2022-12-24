1a96e8f5114bf1b871b5c99cc820289959a6042e
dmittrey <dmitryzubakhin@mail.ru>
2022-12-25 01:31:38 +0300

    Refactor
    Put all methods into FC

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,5 @@
-import { Box, Flex, HStack } from "@chakra-ui/react";
-import { FC, useContext, useEffect, useState } from "react";
+import { Box, Flex, HStack, VStack } from "@chakra-ui/react";
+import { FC, ReactNode, useContext, useEffect, useState } from "react";
 import { DarkModeSwitch } from "../components/utility/DarkModeSwitch";
 import Hero from "../types/Hero";
 import Effect from "../types/Effect";
@@ -14,50 +14,11 @@ import StartButton from "../components/main/StartButton";
 import { StatisticsApi } from "../API/StatisticsApi";
 import { LobbyApi } from "../API/LobbyApi";
 import { AuthContext } from "../App";
+import LobbySocket from "../WS/LobbySocket";
 
-const handleBuyHero = (heroId: number) => {
-  HeroApi.buyHero({ heroId });
-};
-
-const handleBuyEffect = (effectId: number) => {
-  EffectApi.buyEffect({ effectId });
-};
-
-const handleNextHero = (
-  updateFunc: (heroId: number) => void,
-  newHeroId: number
-) => {
-  updateFunc(newHeroId);
-};
-
-const handleNextEffect = (
-  updateFunc: (effectId: number) => void,
-  newEffectId: number
-) => {
-  updateFunc(newEffectId);
-};
-
-const handleRefreshHeroes = (updateFunc: (heroes: Hero[]) => void) => {
-  HeroApi.getAll()
-    .then((heroes) => updateFunc(heroes.data.heroes))
-    .catch((err) => console.log(err));
-};
-
-const handleRefreshEffects = (updateFunc: (effects: Effect[]) => void) => {
-  EffectApi.getAll()
-    .then((effects) => updateFunc(effects.data.effects))
-    .catch((err) => console.log(err));
-};
-
-const handleStart = () => {
-  LobbyApi.createLobby();
-};
-
-interface MainPageProps {}
-
-const MainPage: FC<MainPageProps> = (props) => {
+const MainPage: FC = () => {
   const { userId, username } = useContext(AuthContext);
-  
+
   const [heroesList, setHeroesList] = useState<Hero[]>([]);
   const [effectsList, setEffectsList] = useState<Effect[]>([]);
 
@@ -66,26 +27,141 @@ const MainPage: FC<MainPageProps> = (props) => {
 
   const [statistics, setStatistics] = useState<Statistics>();
 
+  const handleBuyHero = () => {
+    HeroApi.buyHero({ heroId: currentHeroId });
+  };
+
+  const handleBuyEffect = () => {
+    EffectApi.buyEffect({ effectId: currentEffectId });
+  };
+
+  const handleNextHero = () => {
+    if (heroesList[currentHeroId]?.id + 1 === heroesList.length) {
+      setCurrentHeroId(currentHeroId + 1);
+    } else {
+      setCurrentHeroId(0);
+    }
+  };
+
+  const handleNextEffect = () => {
+    if (effectsList[currentEffectId]?.id + 1 === effectsList.length) {
+      setCurrentEffectId(currentEffectId + 1);
+    } else {
+      setCurrentEffectId(0);
+    }
+  };
+
+  const handleRefreshHeroes = () => {
+    console.log("Resfresh");
+    HeroApi.getAll()
+      .then((heroes) => setHeroesList(heroes.data?.heroes))
+      .catch((err) => console.log(err));
+  };
+
+  const handleRefreshEffects = () => {
+    EffectApi.getAll()
+      .then((effects) => setEffectsList(effects.data?.effects))
+      .catch((err) => console.log(err));
+  };
+
+  const handleStart = () => {
+    LobbyApi.createLobby();
+    // LobbySocket.connect();
+  };
+
+  const renderHeroCard = (): ReactNode => {
+    if (heroesList[currentHeroId]) {
+      return (
+        <EntityCard
+          entity={heroesList[currentHeroId]}
+          imgSrc={heroesList[currentHeroId]?.imgSrc}
+          onBuy={handleBuyHero}
+          onNext={handleNextHero}
+          onRefresh={handleRefreshHeroes}
+          defaultText="Heroes not found!"
+        >
+          <HStack>
+            <Box>
+              <FaDollarSign />
+              {heroesList[currentHeroId]?.price}
+            </Box>
+            <Box>
+              <FaHeartbeat />
+              {heroesList[currentHeroId]?.health}
+            </Box>
+          </HStack>
+        </EntityCard>
+      );
+    } else {
+      return (
+        <EntityCard
+          onRefresh={handleRefreshHeroes}
+          defaultText="Heroes not found!"
+        />
+      );
+    }
+  };
+
+  const renderEffectCard = (): ReactNode => {
+    if (effectsList[currentHeroId]) {
+      return (
+        <EntityCard
+          entity={effectsList[currentHeroId]}
+          onBuy={handleBuyEffect}
+          onNext={handleNextEffect}
+          onRefresh={handleRefreshEffects}
+          defaultText="Heroes not found!"
+        >
+          <HStack>
+            <Box w="40px" h="40px" color="#107896">
+              <GiBackstab />
+              {effectsList[currentEffectId]?.stamina}
+            </Box>
+            <Box w="40px" h="40px" color="#C02F1D">
+              <GiHighKick />
+              {effectsList[currentEffectId]?.strength}
+            </Box>
+            <Box w="40px" h="40px" color="orange.500">
+              <GiHolyGrail />
+              {effectsList[currentEffectId]?.luck}
+            </Box>
+            <Box w="40px" h="40px" color="green.400">
+              <GiLeg />
+              {effectsList[currentEffectId]?.constitution}
+            </Box>
+          </HStack>
+        </EntityCard>
+      );
+    } else {
+      return (
+        <EntityCard
+          onRefresh={handleRefreshEffects}
+          defaultText="Effects not found!"
+        />
+      );
+    }
+  };
+
   useEffect(() => {
     HeroApi.getAll()
       .then((response) => {
-        setHeroesList(response.data.heroes);
+        setHeroesList(response.data?.heroes);
         setCurrentHeroId(0);
       })
       .catch((err) => console.log(err));
 
     EffectApi.getAll()
       .then((response) => {
-        setEffectsList(response.data.effects);
+        setEffectsList(response.data.effects ? response.data.effects : []);
         setCurrentEffectId(0);
       })
       .catch((err) => console.log(err));
 
-    StatisticsApi.getStats()
-      .then((response) => {
-        setStatistics(response.data.statistics);
-      })
-      .catch((err) => console.log(err));
+    // StatisticsApi.getStats()
+    //   .then((response) => {
+    //     setStatistics(response.data.statistics);
+    //   })
+    //   .catch((err) => console.log(err));
   }, []);
 
   return (
@@ -96,103 +172,25 @@ const MainPage: FC<MainPageProps> = (props) => {
         top="5"
         right="5"
       />
-      <Flex position="absolute" align="center" top="130" left="20%">
-        <Box w="md" p="2%">
-          <EntityCard
-            entity={heroesList[currentHeroId]}
-            entityType={EntityType.HERO}
-            imgSrc={
-              heroesList[currentHeroId] !== undefined
-                ? heroesList[currentHeroId].imgSrc
-                : undefined
-            }
-            onBuy={
-              heroesList[currentEffectId] !== undefined
-                ? () => handleBuyHero(heroesList[currentHeroId].id)
-                : undefined
-            }
-            onNext={
-              heroesList[currentEffectId] !== undefined
-                ? () => {
-                    handleNextHero(
-                      setCurrentHeroId,
-                      currentHeroId + 1 === heroesList.length
-                        ? 0
-                        : currentHeroId + 1
-                    );
-                  }
-                : undefined
-            }
-            onRefresh={() => handleRefreshHeroes(setHeroesList)}
-          >
-            <HStack>
-              <Box>
-                <FaDollarSign />
-                {/* {heroesList[currentHeroId].price} */}
-              </Box>
-              <Box>
-                <FaHeartbeat />
-                {/* {heroesList[currentHeroId].health} */}
-              </Box>
-            </HStack>
-          </EntityCard>
-        </Box>
-        <Box w="md">
-          <EntityCard
-            entity={effectsList[currentEffectId]}
-            entityType={EntityType.EFFECT}
-            onBuy={
-              effectsList[currentEffectId] !== undefined
-                ? () => handleBuyEffect(effectsList[currentEffectId].id)
-                : undefined
-            }
-            onNext={
-              effectsList[currentEffectId] !== undefined
-                ? () => {
-                    handleNextEffect(
-                      setCurrentEffectId,
-                      currentEffectId + 1 === effectsList.length
-                        ? 0
-                        : currentEffectId + 1
-                    );
-                  }
-                : undefined
-            }
-            onRefresh={() => handleRefreshEffects(setEffectsList)}
-          >
-            {/* ${effectsList[currentEffectId].price} */}
-            <HStack>
-              <Box w="40px" h="40px" color="#107896">
-                <GiBackstab />
-                {effectsList[currentEffectId]
-                  ? effectsList[currentEffectId].stamina
-                  : ""}
-              </Box>
-              <Box w="40px" h="40px" color="#C02F1D">
-                <GiHighKick />
-                {effectsList[currentEffectId]
-                  ? effectsList[currentEffectId].strength
-                  : ""}
-              </Box>
-              <Box w="40px" h="40px" color="orange.500">
-                <GiHolyGrail />
-                {effectsList[currentEffectId]
-                  ? effectsList[currentEffectId].luck
-                  : ""}
-              </Box>
-              <Box w="40px" h="40px" color="green.400">
-                <GiLeg />
-                {effectsList[currentEffectId]
-                  ? effectsList[currentEffectId].constitution
-                  : ""}
-              </Box>
-            </HStack>
-          </EntityCard>
-          <Box pos="absolute" top="650" left="40%" w="20%" alignItems="center" p='5'>
-            <StartButton onPushButton={() => handleStart()}>START</StartButton>
+      <VStack
+        position="absolute"
+        align="center"
+        justify="center"
+        top="130"
+        left="20%"
+      >
+        <Flex>
+          <Box w="md" p="2%">
+            {renderHeroCard()}
           </Box>
+          <Box w="md" p="2%">
+            {renderEffectCard()}
+          </Box>
+        </Flex>
+        <Box top="650" left="40%" w="20%" alignItems="center" p="5">
+          <StartButton onPushButton={() => handleStart()}>START</StartButton>
         </Box>
-      </Flex>
+      </VStack>
       <Box p="5" position="absolute" right="10" top="10" w="200">
         <UserStats statistics={statistics} />
       </Box>
