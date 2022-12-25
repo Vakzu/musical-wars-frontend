d6fc7173d832c2ed57d9bc9f5cc32b2e961daa4b
dmittrey <dmitryzubakhin@mail.ru>
2022-12-25 03:45:25 +0300

    Transition Again

--- src/components/main/UserStats.tsx
+++ src/components/main/UserStats.tsx
@@ -1,5 +1,6 @@
 import {
   Box,
+  Collapse,
   Flex,
   Heading,
   Stat,
@@ -8,55 +9,63 @@ import {
   StatHelpText,
   StatLabel,
   StatNumber,
+  VStack,
+  useColorMode,
 } from "@chakra-ui/react";
 import { FC } from "react";
 import Statistics from "../../types/Statistics";
 
 interface UserStatsProps {
+  isOpen: boolean;
   statistics?: Statistics;
 }
 
 const UserStats: FC<UserStatsProps> = (props) => {
+  const { colorMode } = useColorMode();
+  const isDark = colorMode === "dark";
+
   return props.statistics ? (
-    <Flex flexDirection="column">
-      <Box>
-        <Stat>
-          <StatLabel>
-            <Heading size="md">Played games:</Heading>
-          </StatLabel>
-          <StatNumber>{props.statistics.playedGamesAmount}</StatNumber>
-        </Stat>
-      </Box>
-
-      <Box>
-        <Stat>
-          <StatLabel>
+    <Collapse in={props.isOpen} animateOpacity>
+      <VStack align="left">
+        <Box>
+          <Stat>
             <StatLabel>
-              <Heading size="md">Wins:</Heading>
+              <Heading size="md">Played games:</Heading>
             </StatLabel>
-          </StatLabel>
-          <StatNumber>{props.statistics.winsAmount}</StatNumber>
-        </Stat>
-      </Box>
+            <StatNumber>{props?.statistics.playedGamesAmount}</StatNumber>
+          </Stat>
+        </Box>
 
-      <Box>
-        <Stat>
-          <StatLabel>
-            <Heading size="md">Average place:</Heading>
-          </StatLabel>
-          <StatNumber>{props.statistics.averagePlace}</StatNumber>
-        </Stat>
-      </Box>
+        <Box>
+          <Stat>
+            <StatLabel>
+              <StatLabel>
+                <Heading size="md">Wins:</Heading>
+              </StatLabel>
+            </StatLabel>
+            <StatNumber>{props?.statistics.winsAmount}</StatNumber>
+          </Stat>
+        </Box>
 
-      <Box>
-        <Stat>
-          <StatLabel>
-            <Heading size="md">Last game was:</Heading>
-          </StatLabel>
-          <StatNumber>{props.statistics.lastGameTimeStamp}</StatNumber>
-        </Stat>
-      </Box>
-    </Flex>
+        <Box>
+          <Stat>
+            <StatLabel>
+              <Heading size="md">Average place:</Heading>
+            </StatLabel>
+            <StatNumber>{props?.statistics.averagePlace}</StatNumber>
+          </Stat>
+        </Box>
+
+        <Box>
+          <Stat>
+            <StatLabel>
+              <Heading size="md">Last game was:</Heading>
+            </StatLabel>
+            <StatNumber>{props?.statistics.lastGameTimeStamp}</StatNumber>
+          </Stat>
+        </Box>
+      </VStack>
+    </Collapse>
   ) : (
     <Box></Box>
   );
