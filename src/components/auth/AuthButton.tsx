a171bd647d3cf97ea30e4070c102ee93fe5e4971
dmittrey <dmitryzubakhin@mail.ru>
2022-12-25 07:21:49 +0300

    Change type of auth button to implicit enum

--- src/components/auth/AuthButton.tsx
+++ src/components/auth/AuthButton.tsx
@@ -1,27 +1,28 @@
-import React, {FC, ReactNode} from 'react';
+import React, { FC, ReactNode } from "react";
 
-import {Box, Button} from '@chakra-ui/react'
-
-export enum AuthButtonType {
-    SOLID = 'solid',
-    GHOST = 'ghost'
-}
+import { Box, Button } from "@chakra-ui/react";
 
 interface AuthButtonProps {
-    isLoading?: boolean,
-    onPushButton: () => void,
-    type: AuthButtonType,
-    children: ReactNode
+  isLoading?: boolean;
+  onPushButton: () => void;
+  type: "solid" | "ghost";
+  children: ReactNode;
 }
 
 const AuthButton: FC<AuthButtonProps> = (props) => {
-    return (
-        <Box>
-            <Button isLoading={props.isLoading} colorScheme='teal' variant={props.type} size='lg' onClick={props.onPushButton}>
-                {props.children}
-            </Button>
-        </Box>
-    );
+  return (
+    <Box>
+      <Button
+        isLoading={props.isLoading}
+        colorScheme="teal"
+        variant={props.type}
+        size="lg"
+        onClick={props.onPushButton}
+      >
+        {props.children}
+      </Button>
+    </Box>
+  );
 };
 
 export default AuthButton;
