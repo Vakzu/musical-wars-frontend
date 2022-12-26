4d41a56afa5351e3837148d8b96611eb1ffb5dd8
dmittrey <dmitryzubakhin@mail.ru>
2022-12-26 05:10:14 +0300

    Add corousel with characters in fight page

---
+++ src/components/fight/FightAnimation.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const FightAnimation: FC = () => {
+  return (
+    <div>
+      <img src='./attack_gif.gif' alt="loading..." />
+    </div>
+  );
+};
+
+export default FightAnimation;
