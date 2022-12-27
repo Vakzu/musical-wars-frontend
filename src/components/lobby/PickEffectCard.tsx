5bfca2d79da5a01b40f8481b18c1d21065c20cc3
dmittrey <dmitryzubakhin@mail.ru>
2022-12-27 07:20:40 +0300

    Add effects tooltip in lobby page

--- src/components/lobby/PickEffectCard.tsx
+++ src/components/lobby/PickEffectCard.tsx
@@ -11,6 +11,7 @@ import {
   Text,
   HStack,
   Box,
+  Tooltip,
 } from "@chakra-ui/react";
 import { EffectApi } from "../../API/EffectApi";
 import Effect from "../../types/Effect";
@@ -58,24 +59,40 @@ const PickEffectCard: FC<PickEffectCardProps> = ({ onPick, children }) => {
         <CardBody>
           <Stack mt="3" spacing="3">
             <Heading size="lg">{currentEffect.name}</Heading>
-            <Text color="blue.600" fontSize="2xl">
-              <HStack>
-                <Box w="40px" h="40px" color="#107896">
-                  <GiBackstab />
-                  {currentEffect.stamina}
-                </Box>
-                <Box w="40px" h="40px" color="#C02F1D">
-                  <GiHighKick />
-                  {currentEffect.strength}
-                </Box>
-                <Box w="40px" h="40px" color="orange.500">
-                  <GiHolyGrail />
-                  {currentEffect.luck}
-                </Box>
-                <Box w="40px" h="40px" color="green.400">
-                  <GiLeg />
-                  {currentEffect.constitution}
-                </Box>
+            <Text fontSize="2xl">
+              <HStack spacing="5%">
+                <Tooltip hasArrow placement="top" label="Stamina" fontSize="1.3rem">
+                  <Stack color="#107896" justify="center" align="center">
+                    <Box fontSize="100%">
+                      <GiBackstab />
+                    </Box>
+                    <Box fontSize="100%">{currentEffect.stamina}</Box>
+                  </Stack>
+                </Tooltip>
+                <Tooltip hasArrow placement="top" label="Strength" fontSize="1.3rem">
+                  <Stack color="#C02F1D" justify="center" align="center">
+                    <Box fontSize="100%">
+                      <GiHighKick />
+                    </Box>
+                    <Box fontSize="100%">{currentEffect.strength}</Box>
+                  </Stack>
+                </Tooltip>
+                <Tooltip hasArrow placement="top" label="Luck" fontSize="1.3rem">
+                  <Stack color="orange.500" justify="center" align="center">
+                    <Box fontSize="100%">
+                      <GiHolyGrail />
+                    </Box>
+                    <Box fontSize="100%">{currentEffect.luck}</Box>
+                  </Stack>
+                </Tooltip>
+                <Tooltip hasArrow placement="top" label="Constitution" fontSize="1.3rem">
+                  <Stack color="green.400" justify="center" align="center">
+                    <Box fontSize="100%">
+                      <GiLeg />
+                    </Box>
+                    <Box fontSize="100%">{currentEffect.constitution}</Box>
+                  </Stack>
+                </Tooltip>
               </HStack>
             </Text>
           </Stack>
